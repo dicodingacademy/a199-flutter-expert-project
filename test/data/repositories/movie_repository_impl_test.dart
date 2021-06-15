3f76a73bc9c5dfbbb93f6b7d4de5165c1499d442
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-06-15 10:11:04 +0700

    test: (Search Movies) should return movie list when call to data source is successful

--- test/data/repositories/movie_repository_impl_test.dart
+++ test/data/repositories/movie_repository_impl_test.dart
@@ -275,4 +275,36 @@ void main() {
           equals(Left(ConnectionFailure('Failed to connect to the network'))));
     });
   });
+
+  group('Seach Movies', () {
+    final tMovieModel = MovieModel(
+      adult: false,
+      backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+      genreIds: [14, 28],
+      id: 557,
+      originalTitle: 'Spider-Man',
+      overview:
+          'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+      popularity: 60.441,
+      posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+      releaseDate: '2002-05-01',
+      title: 'Spider-Man',
+      video: false,
+      voteAverage: 7.2,
+      voteCount: 13507,
+    );
+    final tMovieList = <MovieModel>[tMovieModel];
+    final tQuery = 'spiderman';
+
+    test('should return movie list when call to data source is successful',
+        () async {
+      // arrange
+      when(mockRemoteDataSource.searchMovies(tQuery))
+          .thenAnswer((_) async => tMovieList);
+      // act
+      final result = await repository.searchMovies(tQuery);
+      // assert
+      expect(result, Right(tMovieList));
+    });
+  });
 }
