860ed317eaeba67404017f10d480350c3a6a5467
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-06-15 14:00:33 +0700

    implement: should change search result data when data is gotten successfully

--- lib/presentation/provider/movie_search_notifier.dart
+++ lib/presentation/provider/movie_search_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/usecases/search_movies.dart';
 import 'package:flutter/foundation.dart';
 
@@ -10,8 +11,21 @@ class MovieSearchNotifier extends ChangeNotifier {
   RequestState _state = RequestState.Empty;
   RequestState get state => _state;
 
+  List<Movie> _searchResult = [];
+  List<Movie> get searchResult => _searchResult;
+
   Future<void> fetchMovieSearch(String query) async {
     _state = RequestState.Loading;
     notifyListeners();
+
+    final result = await searchMovies.execute(query);
+    result.fold(
+      (l) {},
+      (data) {
+        _searchResult = data;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
   }
 }
