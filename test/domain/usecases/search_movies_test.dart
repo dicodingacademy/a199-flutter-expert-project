acb57bf9633087fe598da6f6871764353b8de248
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-06-15 09:31:58 +0700

    test usecase should get data from repository (failed)

--- test/domain/usecases/search_movies_test.dart
+++ test/domain/usecases/search_movies_test.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/repositories/movie_repository.dart';
 import 'package:ditonton/domain/usecases/search_movies.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'search_movies_test.mocks.dart';
 
@@ -17,4 +19,15 @@ void main() {
   });
 
   final tMovies = <Movie>[];
+  final tQuery = 'Spiderman';
+
+  test('should get list of movies from the repository', () async {
+    // arrange
+    when(mockMovieRepository.searchMovies(tQuery))
+        .thenAnswer((_) async => Right(tMovies));
+    // act
+    final result = await usecase.execute(tQuery);
+    // assert
+    expect(result, Right(tMovies));
+  });
 }
