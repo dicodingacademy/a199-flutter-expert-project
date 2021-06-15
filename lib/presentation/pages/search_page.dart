62c5a1b39932169857606e2a8d732b82b7a6fcd7
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-06-15 16:30:35 +0700

    delete unused import

--- lib/presentation/pages/search_page.dart
+++ lib/presentation/pages/search_page.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/presentation/pages/movie_detail_page.dart';
 import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
 import 'package:flutter/material.dart';
