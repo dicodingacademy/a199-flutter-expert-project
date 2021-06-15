ab02715404a184170580676ab8ed919d9a251801
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-06-15 13:49:04 +0700

    should change state to loading when usecase is called

--- test/presentation/provider/movie_search_notifier_test.dart
+++ test/presentation/provider/movie_search_notifier_test.dart
@@ -1,7 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/usecases/search_movies.dart';
 import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'movie_search_notifier_test.mocks.dart';
 
@@ -18,5 +22,36 @@ void main() {
       ..addListener(() {
         listenerCallCount += 1;
       });
+  });
+
+  final tMovieModel = Movie(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
+  final tMovieList = <Movie>[tMovieModel];
+  final tQuery = 'spiderman';
+
+  group('search movies', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockSearchMovies.execute(tQuery))
+          .thenAnswer((_) async => Right(tMovieList));
+      // act
+      provider.fetchMovieSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loading);
+    });
   });
 }
