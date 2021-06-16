a86af3cada32ec7d2a479949846a5801a4c3ea18
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-06-16 17:19:10 +0700

    implement: should change movies data when data is gotten successfully

--- lib/presentation/provider/popular_movies_notifier.dart
+++ lib/presentation/provider/popular_movies_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/usecases/get_popular_movies.dart';
 import 'package:flutter/foundation.dart';
 
@@ -10,8 +11,22 @@ class PopularMoviesNotifier extends ChangeNotifier {
   RequestState _state = RequestState.Empty;
   RequestState get state => _state;
 
+  List<Movie> _movies = [];
+  List<Movie> get movies => _movies;
+
   Future<void> fetchPopularMovies() async {
     _state = RequestState.Loading;
     notifyListeners();
+
+    final result = await getPopularMovies.execute();
+
+    result.fold(
+      (l) {},
+      (moviesData) {
+        _movies = moviesData;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
   }
 }
