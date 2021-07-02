051f94f193ab058eda64d17910b56d8ca3d3b913
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-07-02 14:35:00 +0700

    add sqflite database helper

---
+++ lib/data/datasources/db/database_helper.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _databaseHelper;
+  DatabaseHelper._instance() {
+    _databaseHelper = this;
+  }
+
+  factory DatabaseHelper() => _databaseHelper ?? DatabaseHelper._instance();
+
+  late Database _database;
+
+  Future<Database> get database async {
+    _database = await _initDb();
+    return _database;
+  }
+
+  static const String _tblWatchlist = 'watchlist';
+
+  Future<Database> _initDb() async {
+    final path = await getDatabasesPath();
+    final databasePath = '$path/ditonton.db';
+
+    var db = await openDatabase(databasePath, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE  $_tblWatchlist (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        overview TEXT,
+        posterPath TEXT,
+        voteAverage REAL,
+      )
+    ''');
+  }
+}
