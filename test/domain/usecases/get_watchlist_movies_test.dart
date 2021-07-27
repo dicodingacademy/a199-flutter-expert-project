eceffcdb39075b563fff5649f7ffa51ac6f2facc
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-07-27 22:08:23 +0700

    test: should get list of movies from the repository

--- test/domain/usecases/get_watchlist_movies_test.dart
+++ test/domain/usecases/get_watchlist_movies_test.dart
@@ -1,6 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
+import '../../dummy_data/dummy_objects.dart';
 import '../../helpers/test_helper.mocks.dart';
 
 void main() {
@@ -10,5 +13,15 @@ void main() {
   setUp(() {
     mockMovieRepository = MockMovieRepository();
     usecase = GetWatchlistMovies(mockMovieRepository);
+  });
+
+  test('should get list of movies from the repository', () async {
+    // arrange
+    when(mockMovieRepository.getWatchlistMovie())
+        .thenAnswer((_) async => Right(testMovieList));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(testMovieList));
   });
 }
--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -1,6 +1,26 @@
 import 'package:ditonton/data/models/movie_detail_table.dart';
 import 'package:ditonton/domain/entities/genre.dart';
+import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/movie_detail.dart';
+
+final testMovie = Movie(
+  adult: false,
+  backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+  genreIds: [14, 28],
+  id: 557,
+  originalTitle: 'Spider-Man',
+  overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  popularity: 60.441,
+  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  releaseDate: '2002-05-01',
+  title: 'Spider-Man',
+  video: false,
+  voteAverage: 7.2,
+  voteCount: 13507,
+);
+
+final testMovieList = [testMovie];
 
 final testMovieDetail = MovieDetail(
   adult: false,
