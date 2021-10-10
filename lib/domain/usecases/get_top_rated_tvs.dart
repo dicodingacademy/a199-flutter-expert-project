4c076abcca8c317a16c4ad3d53a035991cba4435
Tio Irawan <sattanationmail@gmail.com>
2021-10-10 17:19:41 +0700

    Add top rated tvs use case

---
+++ lib/domain/usecases/get_top_rated_tvs.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/repositories/tv_repository.dart';
+
+class GetTopRatedTvs {
+  final TvRepository repository;
+
+  GetTopRatedTvs(this.repository);
+
+  Future<Either<Failure, List<Tv>>> execute() {
+    return repository.getTopRatedTvs();
+  }
+}
