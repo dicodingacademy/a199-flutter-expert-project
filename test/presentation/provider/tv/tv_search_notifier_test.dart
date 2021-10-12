47d059f4fd0c8147adb90ed0e18c4f94246262e0
Tio Irawan <sattanationmail@gmail.com>
2021-10-12 20:16:18 +0700

    Add tv search notifier

---
+++ test/presentation/provider/tv/tv_search_notifier_test.dart
@@ -0,0 +1,83 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/tv/search_tvs.dart';
+import 'package:ditonton/presentation/provider/tv/tv_search_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'tv_search_notifier_test.mocks.dart';
+
+@GenerateMocks([SearchTvs])
+void main() {
+  late TvSearchNotifier provider;
+  late MockSearchTvs mockSearchTvs;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockSearchTvs = MockSearchTvs();
+    provider = TvSearchNotifier(searchTvs: mockSearchTvs)
+      ..addListener(() {
+        listenerCallCount += 1;
+      });
+  });
+
+  final tTvModel = Tv(
+    id: 1,
+    name: 'test',
+    posterPath: 'test',
+    backdropPath: 'test',
+    overview: 'test',
+    firstAirDate: 'test',
+    voteAverage: 1.0,
+    voteCount: 1,
+    genreIds: [1, 2, 3],
+    originCountry: ['test'],
+    originalLanguage: 'test',
+    originalName: 'test',
+    popularity: 1.0,
+  );
+
+  final tTvList = <Tv>[tTvModel];
+  final tQuery = 'spiderman';
+
+  group('search tvs', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockSearchTvs.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvList));
+      // act
+      provider.fetchTvSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loading);
+    });
+
+    test('should change search result data when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockSearchTvs.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvList));
+      // act
+      await provider.fetchTvSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loaded);
+      expect(provider.searchResult, tTvList);
+      expect(listenerCallCount, 2);
+    });
+
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockSearchTvs.execute(tQuery))
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchTvSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
+  });
+}
