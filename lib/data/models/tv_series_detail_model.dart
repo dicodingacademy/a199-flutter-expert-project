7858ea1c638c5f28ec806c1d7a4ce83ae5ffeb8f
unknown <jeremimanogi@gmail.com>
2024-01-02 22:27:13 +0700

    adding equitable in tv series detail model

--- lib/data/models/tv_series_detail_model.dart
+++ lib/data/models/tv_series_detail_model.dart
@@ -1,87 +1,98 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
 import 'package:ditonton/data/models/genre_model.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/tv_series_detail.dart';
 
-class TvSeriesDetailResponse {
-    final bool adult;
-    final String? backdropPath;
-    final List<int> episodeRunTime;
-    final DateTime? firstAirDate;
-    final List<GenreModel> genres;
-    final int id;
-    final bool inProduction;
-    final DateTime? lastAirDate;
-    final TEpisodeToAir lastEpisodeToAir;
-    final String name;
-    final TEpisodeToAir? nextEpisodeToAir;
-    final String originalName;
-    final String overview;
-    final double popularity;
-    final String posterPath;
-    final List<Season> seasons;
-    final String status;
-    final String tagline;
-    final String type;
-    final double voteAverage;
-    final int voteCount;
+class TvSeriesDetailResponse extends Equatable {
+  final bool adult;
+  final String? backdropPath;
+  final List<int> episodeRunTime;
+  final DateTime? firstAirDate;
+  final List<GenreModel> genres;
+  final int id;
+  final bool inProduction;
+  final DateTime? lastAirDate;
+  final TEpisodeToAir lastEpisodeToAir;
+  final String name;
+  final TEpisodeToAir? nextEpisodeToAir;
+  final String originalName;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final List<Season> seasons;
+  final String status;
+  final String tagline;
+  final String type;
+  final double voteAverage;
+  final int voteCount;
 
-    TvSeriesDetailResponse({
-        required this.adult,
-        required this.backdropPath,
-        required this.episodeRunTime,
-        required this.firstAirDate,
-        required this.genres,
-        required this.id,
-        this.inProduction = false,
-        required this.lastAirDate,
-        required this.lastEpisodeToAir,
-        required this.name,
-        required this.nextEpisodeToAir,
-        required this.originalName,
-        required this.overview,
-        required this.popularity,
-        required this.posterPath,
-        required this.seasons,
-        required this.status,
-        required this.tagline,
-        required this.type,
-        required this.voteAverage,
-        required this.voteCount,
-    });
+  TvSeriesDetailResponse({
+    required this.adult,
+    required this.backdropPath,
+    required this.episodeRunTime,
+    required this.firstAirDate,
+    required this.genres,
+    required this.id,
+    this.inProduction = false,
+    required this.lastAirDate,
+    required this.lastEpisodeToAir,
+    required this.name,
+    required this.nextEpisodeToAir,
+    required this.originalName,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    this.seasons = const [],
+    required this.status,
+    required this.tagline,
+    required this.type,
+    required this.voteAverage,
+    required this.voteCount,
+  });
 
-    factory TvSeriesDetailResponse.fromJson(Map<String, dynamic> json) => TvSeriesDetailResponse(
+  factory TvSeriesDetailResponse.fromJson(Map<String, dynamic> json) =>
+      TvSeriesDetailResponse(
         adult: json["adult"],
         backdropPath: json["backdrop_path"],
         episodeRunTime: List<int>.from(json["episode_run_time"].map((x) => x)),
         firstAirDate: DateTime.tryParse(json["first_air_date"]),
-        genres: List<GenreModel>.from(json["genres"].map((x) => GenreModel.fromJson(x))),
+        genres: List<GenreModel>.from(
+            json["genres"].map((x) => GenreModel.fromJson(x))),
         id: json["id"],
         inProduction: json["in_production"],
         lastAirDate: DateTime.tryParse(json["last_air_date"]),
         lastEpisodeToAir: TEpisodeToAir.fromJson(json["last_episode_to_air"]),
         name: json["name"],
-        nextEpisodeToAir: json["next_episode_to_air"] != null ? TEpisodeToAir.fromJson(json["next_episode_to_air"]) : null,
+        nextEpisodeToAir: json["next_episode_to_air"] != null
+            ? TEpisodeToAir.fromJson(json["next_episode_to_air"])
+            : null,
         originalName: json["original_name"],
         overview: json["overview"],
         popularity: json["popularity"]?.toDouble(),
         posterPath: json["poster_path"],
-        seasons: List<Season>.from(json["seasons"].map((x) => Season.fromJson(x))),
+        seasons: json["seasons"] != null
+            ? List<Season>.from(json["seasons"].map((x) => Season.fromJson(x)))
+            : [],
         status: json["status"],
         tagline: json["tagline"],
         type: json["type"],
         voteAverage: json["vote_average"]?.toDouble(),
         voteCount: json["vote_count"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "adult": adult,
         "backdrop_path": backdropPath,
         "episode_run_time": List<dynamic>.from(episodeRunTime.map((x) => x)),
-        "first_air_date": "${firstAirDate?.year.toString().padLeft(4, '0')}-${firstAirDate?.month.toString().padLeft(2, '0')}-${firstAirDate?.day.toString().padLeft(2, '0')}",
+        "first_air_date":
+            "${firstAirDate?.year.toString().padLeft(4, '0')}-${firstAirDate?.month.toString().padLeft(2, '0')}-${firstAirDate?.day.toString().padLeft(2, '0')}",
         "genres": List<dynamic>.from(genres.map((x) => x.toJson())),
         "id": id,
         "in_production": inProduction,
-        "last_air_date": "${lastAirDate?.year.toString().padLeft(4, '0')}-${lastAirDate?.month.toString().padLeft(2, '0')}-${lastAirDate?.day.toString().padLeft(2, '0')}",
+        "last_air_date":
+            "${lastAirDate?.year.toString().padLeft(4, '0')}-${lastAirDate?.month.toString().padLeft(2, '0')}-${lastAirDate?.day.toString().padLeft(2, '0')}",
         "last_episode_to_air": lastEpisodeToAir.toJson(),
         "name": name,
         "next_episode_to_air": nextEpisodeToAir?.toJson(),
@@ -95,7 +106,7 @@ class TvSeriesDetailResponse {
         "type": type,
         "vote_average": voteAverage,
         "vote_count": voteCount,
-    };
+      };
 
   TvSeriesDetail toEntity() {
     return TvSeriesDetail(
@@ -104,7 +115,9 @@ class TvSeriesDetailResponse {
       originalName: originalName,
       adult: adult,
       backdropPath: backdropPath,
-      episodeRunTime: episodeRunTime.isEmpty ? (lastEpisodeToAir.runtime ?? 0) : episodeRunTime.first,
+      episodeRunTime: episodeRunTime.isEmpty
+          ? (lastEpisodeToAir.runtime ?? 0)
+          : episodeRunTime.first,
       genres: genres.map((e) => Genre(id: e.id, name: e.name)).toList(),
       overview: overview,
       popularity: popularity,
@@ -115,40 +128,67 @@ class TvSeriesDetailResponse {
       voteCount: voteCount,
     );
   }
+
+  @override
+  List<Object?> get props {
+    return [
+      adult,
+      backdropPath,
+      episodeRunTime,
+      firstAirDate,
+      genres,
+      id,
+      inProduction,
+      lastAirDate,
+      lastEpisodeToAir,
+      name,
+      nextEpisodeToAir,
+      originalName,
+      overview,
+      popularity,
+      posterPath,
+      seasons,
+      status,
+      tagline,
+      type,
+      voteAverage,
+      voteCount,
+    ];
+  }
 }
 
-class TEpisodeToAir {
-    final int id;
-    final String name;
-    final String overview;
-    final double voteAverage;
-    final int voteCount;
-    final DateTime? airDate;
-    final int episodeNumber;
-    final String episodeType;
-    final String productionCode;
-    final int? runtime;
-    final int seasonNumber;
-    final int showId;
-    final String? stillPath;
+class TEpisodeToAir extends Equatable {
+  final int id;
+  final String name;
+  final String overview;
+  final double voteAverage;
+  final int voteCount;
+  final DateTime? airDate;
+  final int episodeNumber;
+  final String episodeType;
+  final String productionCode;
+  final int? runtime;
+  final int seasonNumber;
+  final int showId;
+  final String? stillPath;
 
-    TEpisodeToAir({
-        required this.id,
-        required this.name,
-        required this.overview,
-        required this.voteAverage,
-        required this.voteCount,
-        this.airDate,
-        required this.episodeNumber,
-        required this.episodeType,
-        required this.productionCode,
-        required this.runtime,
-        required this.seasonNumber,
-        required this.showId,
-        required this.stillPath,
-    });
+  TEpisodeToAir({
+    required this.id,
+    required this.name,
+    required this.overview,
+    required this.voteAverage,
+    required this.voteCount,
+    this.airDate,
+    required this.episodeNumber,
+    required this.episodeType,
+    required this.productionCode,
+    required this.runtime,
+    required this.seasonNumber,
+    required this.showId,
+    required this.stillPath,
+  });
 
-    factory TEpisodeToAir.fromJson(Map<String, dynamic> json) => TEpisodeToAir(
+  factory TEpisodeToAir.fromJson(Map<String, dynamic> json) => TEpisodeToAir(
         id: json["id"],
         name: json["name"],
         overview: json["overview"],
@@ -162,15 +202,16 @@ class TEpisodeToAir {
         seasonNumber: json["season_number"],
         showId: json["show_id"],
         stillPath: json["still_path"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "overview": overview,
         "vote_average": voteAverage,
         "vote_count": voteCount,
-        "air_date": "${airDate?.year.toString().padLeft(4, '0')}-${airDate?.month.toString().padLeft(2, '0')}-${airDate?.day.toString().padLeft(2, '0')}",
+        "air_date":
+            "${airDate?.year.toString().padLeft(4, '0')}-${airDate?.month.toString().padLeft(2, '0')}-${airDate?.day.toString().padLeft(2, '0')}",
         "episode_number": episodeNumber,
         "episode_type": episodeType,
         "production_code": productionCode,
@@ -178,32 +219,54 @@ class TEpisodeToAir {
         "season_number": seasonNumber,
         "show_id": showId,
         "still_path": stillPath,
-    };
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      name,
+      overview,
+      voteAverage,
+      voteCount,
+      airDate,
+      episodeNumber,
+      episodeType,
+      productionCode,
+      runtime,
+      seasonNumber,
+      showId,
+      stillPath,
+    ];
+  }
 }
 
-class Season {
-    final DateTime? airDate;
-    final int episodeCount;
-    final int id;
-    final String name;
-    final String overview;
-    final String? posterPath;
-    final int seasonNumber;
-    final double voteAverage;
+class Season extends Equatable {
+  final DateTime? airDate;
+  final int episodeCount;
+  final int id;
+  final String name;
+  final String overview;
+  final String? posterPath;
+  final int seasonNumber;
+  final double voteAverage;
 
-    Season({
-        required this.airDate,
-        required this.episodeCount,
-        required this.id,
-        required this.name,
-        required this.overview,
-        required this.posterPath,
-        required this.seasonNumber,
-        required this.voteAverage,
-    });
+  Season({
+    required this.airDate,
+    required this.episodeCount,
+    required this.id,
+    required this.name,
+    this.overview = '',
+    required this.posterPath,
+    required this.seasonNumber,
+    required this.voteAverage,
+  });
 
-    factory Season.fromJson(Map<String, dynamic> json) => Season(
-        airDate: json["air_date"] == null ? null : DateTime.parse(json["air_date"]),
+  static Season fromJson(Map<String, dynamic> json) {
+    try {
+      return Season(
+        airDate:
+            json["air_date"] == null ? null : DateTime.parse(json["air_date"]),
         episodeCount: json["episode_count"],
         id: json["id"],
         name: json["name"],
@@ -211,10 +274,16 @@ class Season {
         posterPath: json["poster_path"],
         seasonNumber: json["season_number"],
         voteAverage: json["vote_average"]?.toDouble(),
-    );
+      );
+    } catch (e) {
+      print(e);
+      throw e;
+    }
+  }
 
-    Map<String, dynamic> toJson() => {
-        "air_date": "${airDate!.year.toString().padLeft(4, '0')}-${airDate!.month.toString().padLeft(2, '0')}-${airDate!.day.toString().padLeft(2, '0')}",
+  Map<String, dynamic> toJson() => {
+        "air_date": airDate != null ?
+            "${airDate!.year.toString().padLeft(4, '0')}-${airDate!.month.toString().padLeft(2, '0')}-${airDate!.day.toString().padLeft(2, '0')}" : null,
         "episode_count": episodeCount,
         "id": id,
         "name": name,
@@ -222,5 +291,19 @@ class Season {
         "poster_path": posterPath,
         "season_number": seasonNumber,
         "vote_average": voteAverage,
-    };
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      airDate,
+      episodeCount,
+      id,
+      name,
+      overview,
+      posterPath,
+      seasonNumber,
+      voteAverage,
+    ];
+  }
 }
