9d3ab9b026f2dfe6b6c2f40db03143a534f46ec0
unknown <jeremimanogi@gmail.com>
2024-01-03 15:08:40 +0700

    update watchlist movie and crate watchlist tv series

--- test/presentation/provider/watchlist_tv_series_notifier_test.dart
+++ test/presentation/provider/watchlist_tv_series_notifier_test.dart
@@ -1,49 +1,49 @@
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
-import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
+import 'package:ditonton/domain/usecases/get_watchlist_tv_series.dart';
+import 'package:ditonton/presentation/provider/watchlist_tv_series_notifier.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import '../../dummy_data/dummy_objects.dart';
-import 'watchlist_movie_notifier_test.mocks.dart';
+import 'watchlist_tv_series_notifier_test.mocks.dart';
 
-@GenerateMocks([GetWatchlistMovies])
+@GenerateMocks([GetWatchlistTvSeries])
 void main() {
-  late WatchlistMovieNotifier provider;
-  late MockGetWatchlistMovies mockGetWatchlistMovies;
+  late WatchlistTvSeriesNotifier provider;
+  late MockGetWatchlistTvSeries mockGetWatchlistTvSeries;
   late int listenerCallCount;
 
   setUp(() {
     listenerCallCount = 0;
-    mockGetWatchlistMovies = MockGetWatchlistMovies();
-    provider = WatchlistMovieNotifier(
-      getWatchlistMovies: mockGetWatchlistMovies,
+    mockGetWatchlistTvSeries = MockGetWatchlistTvSeries();
+    provider = WatchlistTvSeriesNotifier(
+      getWatchlistTvSeries: mockGetWatchlistTvSeries,
     )..addListener(() {
         listenerCallCount += 1;
       });
   });
 
-  test('should change movies data when data is gotten successfully', () async {
+  test('should change TvSeries data when data is gotten successfully', () async {
     // arrange
-    when(mockGetWatchlistMovies.execute())
-        .thenAnswer((_) async => Right([testWatchlistMovie]));
+    when(mockGetWatchlistTvSeries.execute())
+        .thenAnswer((_) async => Right([testWatchlistTvSeries]));
     // act
-    await provider.fetchWatchlistMovies();
+    await provider.fetchWatchlistTvSeries();
     // assert
     expect(provider.watchlistState, RequestState.Loaded);
-    expect(provider.watchlist, [testWatchlistMovie]);
+    expect(provider.watchlist, [testWatchlistTvSeries]);
     expect(listenerCallCount, 2);
   });
 
   test('should return error when data is unsuccessful', () async {
     // arrange
-    when(mockGetWatchlistMovies.execute())
+    when(mockGetWatchlistTvSeries.execute())
         .thenAnswer((_) async => Left(DatabaseFailure("Can't get data")));
     // act
-    await provider.fetchWatchlistMovies();
+    await provider.fetchWatchlistTvSeries();
     // assert
     expect(provider.watchlistState, RequestState.Error);
     expect(provider.message, "Can't get data");
