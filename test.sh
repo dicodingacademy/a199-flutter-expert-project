950bfeae70894413c36c1e03a5d9075e9fa8ae24
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2024-03-07 16:34:50 +0700

    update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 error=false
 
 show_help() {
-    printf "usage: $0 [--help]
+  printf "usage: $0 [--help]
 Tool for running all unit and widget tests with code coverage and automatically generated if lcov is installed.
 
 (run from root of repo)
@@ -14,12 +14,12 @@ where:
     --help
         print this message
 "
-    exit 1
+  exit 1
 }
 
 # run unit and widget tests
-runTests () {
-  cd $1;
+runTests() {
+  cd $1
   if [ -f "pubspec.yaml" ] && [ -d "test" ]; then
     echo "running tests in $1"
     flutter pub get
@@ -27,65 +27,71 @@ runTests () {
     escapedPath="$(echo $1 | sed 's/\//\\\//g')"
 
     # run tests with coverage
-    if grep flutter pubspec.yaml > /dev/null; then
+    if grep flutter pubspec.yaml >/dev/null; then
       echo "run flutter tests"
       if [ -f "test/all_tests.dart" ]; then
         flutter test --coverage test/all_tests.dart || error=true
       else
         flutter test --coverage || error=true
       fi
+
       if [ -d "coverage" ]; then
         # combine line coverage info from package tests to a common file
-        sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> $2/coverage/lcov.info
+        sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >>$2/coverage/test.info
+        rm -f coverage/lcov.info
       fi
     else
       echo "not a flutter package, skipping"
     fi
   fi
-  cd - > /dev/null
+  cd - >/dev/null
 }
 
 runReport() {
-    if [ -f "coverage/lcov.info" ] && ! [ "$TRAVIS" ]; then
-        genhtml coverage/lcov.info -o coverage --no-function-coverage -s -p `pwd`/coverage
-        
-		if $IsWindows || $ENV:OS; then
-			start coverage/index.html
-		else
-			open coverage/index.html
-		fi
+  if [ -f "coverage/test.info" ] && ! [ "$TRAVIS" ]; then
+    genhtml coverage/test.info -o coverage --no-function-coverage --prefix $(pwd)
+
+    if [ "$(uname)" == "Darwin" ]; then
+      open coverage/index.html
+    else
+      start coverage/index.html
     fi
+  fi
 }
 
-if ! [ -d .git ]; then printf "\nError: not in root of repo"; show_help; fi
+if ! [ -f "pubspec.yaml" ] && [ -d .git ]; then
+  printf "\nError: not in root of repo\n"
+  show_help
+fi
 
 case $1 in
-    --help)
-        show_help
-        ;;
-    *)
-        currentDir=`pwd`
-        # if no parameter passed
-        if [ -z $1 ]; then
-            rm -f coverage/lcov.info
-            dirs=(`find . -maxdepth 2 -type d`)
-            for dir in "${dirs[@]}"; do
-                runTests $dir $currentDir
-            done
-        else
-            if [[ -d "$1" ]]; then
-                runTests $1 $currentDir
-            else
-                printf "\nError: not a directory: $1"
-                show_help
-            fi
-        fi
-        runReport
-        ;;
+--help)
+  show_help
+  ;;
+*)
+  currentDir=$(pwd)
+  # if no parameter passed
+  if [ -z $1 ]; then
+    if [ -d "coverage" ]; then
+      rm -r coverage
+    fi
+    dirs=($(find . -maxdepth 2 -type d))
+    for dir in "${dirs[@]}"; do
+      runTests $dir $currentDir
+    done
+  else
+    if [[ -d "$1" ]]; then
+      runTests $1 $currentDir
+    else
+      printf "\nError: not a directory: $1"
+      show_help
+    fi
+  fi
+  runReport
+  ;;
 esac
 
 # Fail the build if there was an error
-if [ "$error" = true ] ;
-then
-    exit -1
+if [ "$error" = true ]; then
+  exit -1
 fi
